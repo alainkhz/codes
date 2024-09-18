5eb35687394ebe8d52143608757666b583d1239c
alainkhz <khzzix0131@gmail.com>
2024-09-18 23:10:38 +0800

    1

--- fcuOJ/3.最大公因數與最小公倍數搜尋器/3.c
+++ fcuOJ/3.最大公因數與最小公倍數搜尋器/3.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
+int gcd(int x ,int y){
+    if(y==0)
+    {
+        return x;
+    }
+    else 
+    {
+        return gcd(y,x%y);
+    }
+}
 
+int lcm (int x , int y)
+{
+    return x*y/gcd(x,y);
+}
 
 int main(){
-    printf("test");
-
+    int n1,n2;
+    scanf("%d",&n1);
+    scanf("%d",&n2);
+    printf("%d",gcd(n1,n2));
+    printf(" %d",lcm(n1,n2));
+    return 0;
 }
