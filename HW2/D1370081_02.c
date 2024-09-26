993818b256bcb54da07f0e70fcf9447e0c1f11ac
olan <71214316+alainkhz@users.noreply.github.com>
2024-09-26 12:05:33 +0800

    Add files via upload

---
+++ HW2/D1370081_02.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,c,x,ans;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    scanf("%d",&x);
+    printf("%d",a*x*x+b*x+c);
+    return 0;
+}
